81d29aa210010f516f4743828a2b966155b88ede
fllay <mat38.rlo@gmail.com>
2023-01-30 17:30:18 +0100

    finish

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 from random import randint
+import time
 
 def tableau(x):
 
     tab = [[0] * (x + 2)]+[[0]+[randint(0,1) for largeur in range(x)]+[0] for hauteur in range(x - 2)] + [[0] * (x + 2)]
     return tab
 
-#def affiche1(tab):
-#    for indice in range(len(tab)):
-#        print(tab[indice])
+def affiche1(tab):
+    for indice in range(len(tab)):
+        print(tab[indice])
 
 def affiche2(tab):
     for i in range(1,len(tab)):
@@ -21,6 +22,7 @@ def affiche2(tab):
 
 def verifCases(tab, hauteur, largeur):
     total = 0
+    cellule = tab[largeur][hauteur]
     if tab[largeur-1][hauteur-1] == 1:
         total += 1
     if tab[largeur][hauteur-1] == 1:
@@ -39,9 +41,30 @@ def verifCases(tab, hauteur, largeur):
         total += 1
     if tab[largeur+1][hauteur+1] == 1:
         total += 1
-    print(total)
-    return total
 
-a = tableau(6)
-affiche2(a)
-verifCases(a, 3, 3)
+    if total == 3:
+        cellule = 1
+    elif total <2 or total >3:
+        cellule = 0
+    return cellule
+
+def etapeSuivante(tab, x):
+    nvTableau = [[0] * (x + 2)]+[[0]+[randint(0,1) for largeur in range(x)]+[0] for hauteur in range(x - 2)] + [[0] * (x + 2)]
+    for largeur in range (1,len(tab)-1):
+        for hauteur in range(1,len(tab[largeur])-1):
+            nvTableau[largeur][hauteur] = verifCases(tab, hauteur, largeur)
+    return nvTableau
+
+
+
+def start(x):
+    a = tableau(x)
+    affiche2(a)
+    verif = [[0] * (x + 2)]+[[0]+[randint(0,0) for largeur in range(x)]+[0] for hauteur in range(x - 2)] + [[0] * (x + 2)]
+    while a != verif:
+        print("\n")
+        a = etapeSuivante(a, x)
+        affiche2(a)
+        time.sleep(0.8)
+
+start(10)
